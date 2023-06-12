9ed798b8e805d84cf50515ae47ec69b9ab06eb83
Jen Reiss <jenc.reiss24@gmail.com>
2023-06-12 09:54:32 -0500

    update buildInputs

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
 
           devShells.profiling = mkShell {
             buildInputs = developmentDependencies ++
-              [ libcxx llvmPackages.libclang ];
+              [ clang libcxx libclang ];
             shellHook = ''
               export LIBCLANG_PATH="${LIBCLANG_PATH}"
               export BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS}"
